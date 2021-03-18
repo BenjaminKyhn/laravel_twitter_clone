fe518a37fcb87ef95344989ee9c7f2d125fbec4d
Benjamin Kyhn <benkyhn@hotmail.com>
2021-03-18 20:07:34 +0100

    Part 59

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('/tweets', [TweetController::class, 'store']);
+Route::middleware('auth')->group(function (){
+    Route::get('/tweets', [TweetController::class, 'index'])->name('home');
+    Route::post('/tweets', [TweetController::class, 'store']);
+});
 
 Auth::routes();
-
-Route::get('/home', [HomeController::class, 'index'])->name('home');
