2d11047e6dbcd98e0db02d3814e0138c60a95854
Benjamin Kyhn <benkyhn@hotmail.com>
2021-03-21 19:43:51 +0100

    Part 66

--- resources/views/_sidebar-links.blade.php
+++ resources/views/_sidebar-links.blade.php
@@ -18,49 +18,20 @@
     <li>
         <a
             class="font-bold text-lg mb-4 block"
-            href="#"
-        >
-            Notifications
-        </a>
-    </li>
-    <li>
-        <a
-            class="font-bold text-lg mb-4 block"
-            href="#"
-        >
-            Messages
-        </a>
-    </li>
-    <li>
-        <a
-            class="font-bold text-lg mb-4 block"
-            href="#"
-        >
-            Bookmarks
-        </a>
-    </li>
-    <li>
-        <a
-            class="font-bold text-lg mb-4 block"
-            href="#"
-        >
-            Lists
-        </a>
-    </li>
-    <li>
-        <a
-            class="font-bold text-lg mb-4 block"
             href="{{ route('profile', auth()->user()) }}"
         >
             Profile
         </a>
     </li>
     <li>
-        <a
-            class="font-bold text-lg mb-4 block"
-            href="#"
+        <form
+            method="POST"
+            action="/logout"
         >
-            More
-        </a>
+            @csrf
+
+            <button class="font-bold text-lg">Logout</button>
+        </form>
+
     </li>
 </ul>
