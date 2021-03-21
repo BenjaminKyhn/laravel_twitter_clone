7d45ba5aba1673eb355051f2032d023b0c5ad574
Benjamin Kyhn <benkyhn@hotmail.com>
2021-03-21 17:06:22 +0100

    Part 63

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,4 +56,10 @@ class User extends Authenticatable
     public function getAvatarAttribute(){
         return "https://i.pravatar.cc/200?u=" . $this->email;
     }
+
+    public function path($append = ''){
+        $path = route('profile', $this->name);
+
+        return $append ? "{$path}/{$append}" : $path;
+    }
 }
