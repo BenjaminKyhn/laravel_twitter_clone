5949333d83443468a4178831eb5db8b2f54efb8e
Benjamin Kyhn <benkyhn@hotmail.com>
2021-03-21 16:46:25 +0100

    Part 62

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -1,29 +1,32 @@
 <x-app>
 <header class="mb-6 relative">
-    <img
-        src="/images/default-profile-banner.jpg"
-        alt=""
-        class="mb-2"
-    >
+    <div class="relative">
+        <img
+            src="/images/default-profile-banner.jpg"
+            alt=""
+            class="mb-2"
+        >
 
-    <div class="flex justify-between items-center mb-4">
+        <img
+            src="{{ $user->avatar }}"
+            alt=""
+            class="rounded-full mr-2 bottom-0 transform -translate-x-1/2 translate-y-1/2"
+            style="left: 50%; width: 150px; position: absolute;"
+        >
+    </div>
+
+    <div class="flex justify-between items-center mb-6">
         <div>
             <h2 class="font-bold text-2xl mb-0">{{ $user->name }}</h2>
             <p class="text-sm">Joined {{ $user->created_at->diffForHumans() }}</p>
         </div>
 
-        <div>
+        <div class="flex">
             <a href="" class="rounded-full border border-gray-300 py-2 px-4 text-xs mr-2">Edit Profile</a>
-            <a href="" class="bg-blue-500 rounded-full shadow py-2 px-4 text-white text-xs">Follow Me</a>
+
+            <x-follow-button :user="$user"></x-follow-button>
         </div>
     </div>
-
-    <img
-        src="{{ $user->avatar }}"
-        alt=""
-        class="rounded-full mr-2"
-        style="width: 150px; left: calc(50% - 75px); top: 138px; position: absolute;"
-    >
 
     <p class="text-sm">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus et risus quis nisi aliquam blandit pretium at libero. Maecenas vehicula tristique felis. Etiam sagittis, dolor non sagittis vulputate, metus ex maximus ante, in hendrerit purus diam blandit leo. Phasellus id erat odio. Aliquam dignissim urna eu placerat accumsan. Nullam aliquam varius ipsum, eget hendrerit nibh posuere id. Sed lobortis dapibus turpis, in maximus est consequat quis. Donec nisl purus, gravida et fermentum a, vulputate nec libero. Curabitur quis erat eu purus consequat finibus ac ac lacus. Phasellus sed consequat dui, eu egestas ligula. Nunc venenatis eu turpis nec finibus. Aenean vestibulum vestibulum justo ac interdum.
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FollowsController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfilesController;
 use App\Http\Controllers\TweetController;
@@ -24,8 +25,9 @@ Route::get('/', function () {
 Route::middleware('auth')->group(function (){
     Route::get('/tweets', [TweetController::class, 'index'])->name('home');
     Route::post('/tweets', [TweetController::class, 'store']);
+    Route::post('/profiles/{user:name}/follow', [FollowsController::class, 'store']);
 });
 
-Route::get('/profiles/{user}', [ProfilesController::class, 'show'])->name('profile');
+Route::get('/profiles/{user:name}', [ProfilesController::class, 'show'])->name('profile');
 
 Auth::routes();
